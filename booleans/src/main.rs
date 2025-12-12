f6a94804def8b045ea5eb01eb6c17e790f00e1a0
Vedansh Saini <vedanshsaini06@gmail.com>
2025-12-12 12:14:56 +0000

    feat: add rust boolean examples with conditional checks

---
+++ booleans/src/main.rs
@@ -0,0 +1,16 @@
+//* Booleans
+
+fn main() {
+    let is_male = false;
+    let is_above_18 = true;
+
+    if is_male {
+        println!("You are a male");
+    } else {
+        println!("You are not a male");
+    }
+
+    if is_male && is_above_18 {
+        print!("You are a legal male");
+    }
+}
