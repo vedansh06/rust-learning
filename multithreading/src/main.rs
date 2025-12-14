7b5c303599d6706f86fe3f88da29158372f098cf
Vedansh Saini <vedanshsaini06@gmail.com>
2025-12-14 14:57:43 +0000

    add multithreading examples using std::thread

---
+++ multithreading/src/main.rs
@@ -0,0 +1,35 @@
+// Multithreading
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let sum = 0;
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }    
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+
+
+use std::thread;
+
+fn main() {
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+    
+    handle.join().unwrap();
+}
